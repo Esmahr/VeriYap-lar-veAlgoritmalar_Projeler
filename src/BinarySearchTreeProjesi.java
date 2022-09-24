a19b05fee4228f83707a2679e8a29d4f302b7a08
Esma Harmancı <92969169+Esmahr@users.noreply.github.com>
2022-09-24 11:03:25 +0300

    BinarySearchTreeProjesi Eklendi

---
+++ src/BinarySearchTreeProjesi.java
@@ -0,0 +1,20 @@
+public class BinarySearchTreeProjesi {
+
+    /*
+    [7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız.
+
+    * root 8'dir.
+
+                  8           -> 7, 8 den kucuk oldugu icin sola, 9 buyuk oldugu icin saga yazildi
+                /   \
+               7     9
+              /               -> 5, 7'den kucuk oldugundan sola yazilir
+             5
+            /   \             -> 1, 5'den kucuk oldugu icin sola, 6 buyuk oldugu icin saga yazildi
+           1     6
+         /   \                -> 3, 5 ve 7 den kucuk ve 1den buyuk oldugundan 1'in sagina yazildi
+        0     3
+             / \              -> 2, 3'den kucuk oldugu icin sola, 4 buyuk oldugu icin saga yazildi
+            2   4
+     */
+}
